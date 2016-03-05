b06fd0400e70debc8b528977e7525f212c871434
phiaseitz <sophia.seitz@students.olin.edu>
2016-03-05 10:37:24 -0500

    Reorganized code from example app to our auth organization

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,5 +1,5 @@
 // Create the controller. This is what controls the app. 
-var wiki = angular.module('mashup', ['ngMaterial', 'spotify'])
+var wiki = angular.module('mashup', ['ngMaterial'])
 .controller('AppCtrl', function ($scope, $log,  $http) {
     
 })
