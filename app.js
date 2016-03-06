76d9a7cf6e52822c7e7ac6f2433df160bc2a4e66
phiaseitz <sophia.seitz@students.olin.edu>
2016-03-06 13:59:55 -0500

    Updated backend for angular

--- app.js
+++ app.js
@@ -23,86 +23,50 @@ var auth           = require('./authentication.js');
 var session        = require('express-session');
 var MongoStore     = require('connect-mongo')(session);
 
-//Modules from spotify authexample
-var swig           = require('swig');
-var methodOverride = require('method-override');
-var consolidate    = require('consolidate');
-
-
 // CONFIGURATION ===============================================================
 app.use(logger('dev'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(methodOverride());
-app.use(bodyParser())
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//FROM EXAMPLE -- WILL NEED TO REMOVE
-app.engine('html', consolidate.swig);
-
-
 
 // CONNECT TO DATABASE =========================================================
-// mongoose.connect('mongodb://localhost/gitify');
+mongoose.connect('mongodb://localhost/gitify');
 
 // SECURITY CONFIGURATION ======================================================
 var passport = auth.configure();
-// app.use(session({
-//   // store: new MongoStore({
-//   //   mongooseConnection: mongoose.connection,
-//   //   ttl: 24 * 60 * 60
-//   // }),
-//   secret: 'gitifysecretkey',
-//   // resave: true,
-//   // saveUninitialized: true
-// }));
-app.use(session({secret: "this is a secret"}));
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60
+  }),
+  secret: 'gitifysecretkey',
+  resave: true,
+  saveUninitialized: true
+}));
+
 app.use(passport.initialize());
 app.use(passport.session())
 
 // ROUTES ======================================================================
 
 // GET requests
-
-app.get('/', function(req, res){
-  res.render('index.html', { user: req.user });
-});
-
-app.get('/account', auth.ensureAuthenticated, function(req, res){
-  res.render('account.html', { user: req.user });
-});
-
-app.get('/login', function(req, res){
-  res.render('login.html', { user: req.user });
-});
-
-// GET /auth/spotify
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request. The first step in spotify authentication will involve redirecting
-//   the user to spotify.com. After authorization, spotify will redirect the user
-//   back to this application at /auth/spotify/callback
 app.get('/auth/spotify', auth.spotifyAuth);
 
-// GET /auth/spotify/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request. If authentication fails, the user will be redirected back to the
-//   login page. Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
 app.get('/auth/spotify/callback', auth.callback);
 
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
+app.get('/logout', auth.logout);
+
+app.get('/api/getAuthenticated', auth.sendAuthenticaed)
 
 // AngularJS requests
-// app.get('/', function (req, res) {
-//   res.sendFile(__dirname + '/public/index.html');
-// });
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 // START SERVER ================================================================
 var PORT = process.env.PORT || 3000;
