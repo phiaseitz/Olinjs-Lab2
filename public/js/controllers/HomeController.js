a0cb9ca3475f55f4d665856c6fd21fade63186e3
phiaseitz <sophia.seitz@students.olin.edu>
2016-03-07 09:50:32 -0500

    Getting all the tracks in a user's playlist

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -3,8 +3,21 @@
 app.controller('HomeController', function($scope, authentication, Spotify) {
 	console.log(authentication)
 
+	$scope.currentPlaylist = {};
+
+	// Spotify Auth stuff. This is the authentication token we get when we 
+	// authenticate with passport-spotify. 
 	Spotify.setAuthToken(authentication.user.accessToken)
+
+	// Get the user's playlists. 
 	Spotify.getUserPlaylists(authentication.user.id).then(function (data){
-		console.log(data);
+		$scope.userPlaylists = data.items;
 	});
+
+	$scope.showPlaylist = function(playlist) {
+		Spotify.getPlaylist(playlist.owner.id, playlist.id).then(function(data){
+			console.log(data);
+			$scope.currentPlaylist = data;
+		})
+	}
 });
