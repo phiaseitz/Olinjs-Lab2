f64a4eea144d44c4a2f4b47ad5a8f9061492c6b3
phiaseitz <sophia.seitz@students.olin.edu>
2016-03-10 10:56:09 -0500

    auth on landing page

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -3,54 +3,59 @@ app.controller('HomeController', function($scope, AuthService, PlaylistService, 
 	$scope.currentPlaylist = {};
 
 	$scope.authenticated = AuthService.permissions.authenticated;
+	console.log($scope.authenticated);
 
-	//Get the playlists that the user is tracking!
-	PlaylistService.getPlaylists(AuthService.permissions.user.id).then(function(playlists){
-		var playlistIds = playlists.map(function(playlist){
-			return playlist.id;
-		});
+	if ($scope.authenticated){
+		//Get the playlists that the user is tracking!
+		PlaylistService.getPlaylists(AuthService.permissions.user.id).then(function(playlists){
+			var playlistIds = playlists.map(function(playlist){
+				return playlist.id;
+			});
 
-		$scope.trackedPlaylists = playlistIds || [];
+			$scope.trackedPlaylists = playlistIds || [];
 
-		console.log($scope.trackedPlaylists)
+			console.log($scope.trackedPlaylists)
 
-		// Spotify Auth stuff. This is the authentication token we get when we 
-		// authenticate with passport-spotify. 
-		Spotify.setAuthToken(AuthService.permissions.user.accessToken);
+			// Spotify Auth stuff. This is the authentication token we get when we 
+			// authenticate with passport-spotify. 
+			Spotify.setAuthToken(AuthService.permissions.user.accessToken);
 
-		// Get the user's playlists. 
-		Spotify.getUserPlaylists(AuthService.permissions.user.id).then(function (data){
-			
-			data.items.forEach(function (playlist){
-				Spotify.getPlaylist(playlist.owner.id, playlist.id).then(function(populatedPlaylist){
-					playlist.tracks = populatedPlaylist.tracks;
-					// console.log(populatedPlaylist.tracks)
+			// Get the user's playlists. 
+			Spotify.getUserPlaylists(AuthService.permissions.user.id).then(function (data){
+				
+				data.items.forEach(function (playlist){
+					Spotify.getPlaylist(playlist.owner.id, playlist.id).then(function(populatedPlaylist){
+						playlist.tracks = populatedPlaylist.tracks;
+						// console.log(populatedPlaylist.tracks)
 
-					//if the playlist is a currently tracked playlist, add that to the playlist object
-					if ($scope.trackedPlaylists.indexOf(playlist.id) >= 0){
-						playlist.isTracked = true;
+						//if the playlist is a currently tracked playlist, add that to the playlist object
+						if ($scope.trackedPlaylists.indexOf(playlist.id) >= 0){
+							playlist.isTracked = true;
 
-						PlaylistService.savePlaylist(playlist.id, getPlaylistSongs(populatedPlaylist)).then(function(response){
-							console.log("saved!");
-						});
-					} else {
-						playlist.isTracked = false;
-					}	
+							PlaylistService.savePlaylist(playlist.id, getPlaylistSongs(populatedPlaylist)).then(function(response){
+								console.log("saved!");
+							});
+						} else {
+							playlist.isTracked = false;
+						}	
+					});
 				});
-			});
-			
-			$scope.userPlaylists = data.items.sort(function (a,b){
-				if(a.name > b.name) {
-					return 1;
-				} else if (a.name < b.name) {
-					return -1;
-				} else {
-					return 0;
-				}
-			});
+				
+				$scope.userPlaylists = data.items.sort(function (a,b){
+					if(a.name > b.name) {
+						return 1;
+					} else if (a.name < b.name) {
+						return -1;
+					} else {
+						return 0;
+					}
+				});
 
-		});
-	})
+			});
+		})
+	}
+
+	
 
 	$scope.trackPlaylist = function(playlist){
 		PlaylistService.trackPlaylist(playlist.id, getPlaylistSongs(playlist)).then(function(response){
