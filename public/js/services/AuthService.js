720e64f26ac9d6ccc3339b53728892cde405ea8a
Sarah Walters <swalters4925@gmail.com>
2016-04-16 13:57:36 -0400

    Lab 2 feedback

--- public/js/services/AuthService.js
+++ public/js/services/AuthService.js
@@ -1,6 +1,6 @@
 // public/javascripts/services/AuthService.js
-// This is the service that we use to handle the communication of authentication from the backend (where we are using passport-spotify for 
-// authentication) and the frontend (where we need an auth token to use the spotify API). 
+// This is the service that we use to handle the communication of authentication from the backend (where we are using passport-spotify for
+// authentication) and the frontend (where we need an auth token to use the spotify API).
 
 var routeForUnauthorizedAccess = '/login';
 
@@ -46,9 +46,9 @@ app.service('AuthService', function($http, $q, $rootScope, $location) {
             deferred.resolve();
         });
       }
-      deferred.resolve();
+      deferred.resolve(); // should this be in an else?
     });
-    return deferred.promise;
+    return deferred.promise; // elegant use of promises!
   };
 
 });
--- public/js/services/PlaylistService.js
+++ public/js/services/PlaylistService.js
@@ -1,6 +1,10 @@
-//This is the service that handles talking to our database from the frontend. We've defined functions that we can call to interact with routes that 
+//This is the service that handles talking to our database from the frontend. We've defined functions that we can call to interact with routes that
 // manage our playlist version control. The functions should be fairly self-explanatory
 app.service('PlaylistService', function($http, $q) {
+  // This is basically an API wrapper -- provides you w/ clientside methods you can call
+  // which talk to your server. You might also see factories used for this purpose, instead of services --
+  // the two are almost identical, but factories are a little more flexible.
+  // Here's an explanation I like: http://weblogs.asp.net/dwahlin/using-an-angularjs-factory-to-interact-with-a-restful-service
 
   this.requests = {
     GET: $http.get,
